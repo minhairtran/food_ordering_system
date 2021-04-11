d418d19d582d35f5d9866c7728d60832cdabb0ab
Trần Minh Hải <minhairtran@gmail.com>
2021-04-11 22:30:04 +0700

    edit code

--- data/pad_sequence_data.py
+++ data/pad_sequence_data.py
@@ -5,21 +5,18 @@ import torch
 import numpy as np
 
 YES_DATASET_PATH = "confirming_data/data_yes.pt"
-NO_DATASET_PATH = "confirming_data/data_no.pt"
-# NOISE_DATASET_PATH = "confirming_data/data_noise.pt"
+# NO_DATASET_PATH = "confirming_data/data_no.pt"
+NOISE_DATASET_PATH = "confirming_data/data_noise.pt"
 SAVED_FILE = "confirming_data/data.pt"
 
 def pad_sequence(first_set, second_set):
     first_set["mel_spectrogram"].tolist().append(second_set["mel_spectrogram"].tolist())
     first_set["mel_spectrogram"] = nn.utils.rnn.pad_sequence(first_set["mel_spectrogram"].tolist(), batch_first=True)
 
-    print("OK")
+    first_set["labels"].tolist().append(second_set["labels"].tolist())
+    first_set["labels"] = nn.utils.rnn.pad_sequence(first_set["labels"].tolist(), batch_first=True)
 
-    # first_set["labels"].tolist().append(second_set["labels"].tolist())
-    # first_set["labels"] = nn.utils.rnn.pad_sequence(first_set["labels"].tolist(), batch_first=True)
-
-    # return first_set["mel_spectrogram"], first_set["labels"]
-    return first_set["mel_spectrogram"]
+    return first_set["mel_spectrogram"], first_set["labels"]
 
 if __name__ == "__main__":
     data = {
@@ -30,17 +27,12 @@ if __name__ == "__main__":
     }
 
     yes_data = torch.load(YES_DATASET_PATH)
-    no_data = torch.load(NO_DATASET_PATH)
+    noise_data = torch.load(NOISE_DATASET_PATH)
+    data["mel_spectrogram"], data["labels"] = pad_sequence(yes_data, noise_data)
 
-    # data["mel_spectrogram"], data["labels"] = pad_sequence(yes_data, no_data)
+    data["label_lengths"] = torch.Tensor(yes_data["label_lengths"].tolist().append(noise_data["label_lengths"].tolist()))
 
-    data["mel_spectrogram"] = pad_sequence(yes_data, no_data)
-
-    data["labels"] = torch.Tensor(yes_data["labels"].tolist().append(no_data["labels"].tolist()))
-
-    data["label_lengths"] = torch.Tensor(yes_data["label_lengths"].tolist().append(no_data["label_lengths"].tolist()))
-
-    data["input_lengths"] = torch.Tensor(yes_data["input_lengths"].tolist().append(no_data["input_lengths"].tolist()))
+    data["input_lengths"] = torch.Tensor(yes_data["input_lengths"].tolist().append(noise_data["input_lengths"].tolist()))
 
     torch.save(data, SAVED_FILE)
 
