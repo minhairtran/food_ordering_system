6c3e2fd65bcf36e95267aaccbd253962d73b6bfd
Trần Minh Hải <minhairtran@gmail.com>
2021-04-16 11:55:41 +0700

    fix bug

--- train/text_transform.py
+++ train/text_transform.py
@@ -55,7 +55,7 @@ class FoodNumberTextTransform:
         """
         self.char_map = {}
         self.index_map = {}
-        for line in confirm_model_char_map_str.strip().split('\n'):
+        for line in food_number_char_map_str.strip().split('\n'):
             ch, index = line.split()
             self.char_map[ch] = int(index)
             self.index_map[int(index)] = ch
