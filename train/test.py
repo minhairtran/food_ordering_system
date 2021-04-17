ddaad4fd1b456a7cbbdc9c46c8da8d9fddeba0e4
Trần Minh Hải <minhairtran@gmail.com>
2021-04-17 11:54:18 +0700

    debug

--- train/test.py
+++ train/test.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     device = torch.device("cuda:1" if use_cuda else "cpu")
 
         model = SpeechRecognitionModel(SpeechRecognitionModel.hparams['n_rnn_layers'], SpeechRecognitionModel.hparams['rnn_dim'], \
-        6, SpeechRecognitionModel.hparams['n_feats'], SpeechRecognitionModel.hparams['dropout']).to(device)
+            6, SpeechRecognitionModel.hparams['n_feats'], SpeechRecognitionModel.hparams['dropout']).to(device)
 
     try:
         checkpoint = torch.load(SAVED_MODEL_PATH)
