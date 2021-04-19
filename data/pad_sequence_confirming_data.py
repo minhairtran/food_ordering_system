79eabaf16f5edcc3f11d295395b51253d1bffa64
Trần Minh Hải <minhairtran@gmail.com>
2021-04-19 16:53:02 +0700

    debug food dataset

--- data/pad_sequence_confirming_data.py
+++ data/pad_sequence_confirming_data.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
             "input_lengths": []
         }
         for current_dataset_index in range(average_dataset_length):
-            original_dataset_index = int(current_dataset_index/len(data_set))
+            original_dataset_index = int(current_dataset_index/len(data_set)) + average_dataset_length*set_number/len(data_set)
             original_dataset_number = current_dataset_index % len(data_set)
 
             if (original_dataset_index >= len(data_set[original_dataset_number]["label_lengths"])):
--- data/pad_sequence_food_number_data.py
+++ data/pad_sequence_food_number_data.py
@@ -42,8 +42,6 @@ if __name__ == "__main__":
 
     average_dataset_length = int(all_data_length/len(data_set))
 
-    print("Length big data set", len(data_set[0]['labels']))
-
     for set_number in range(len(data_set)):
         saved_dataset = {
             "label_lengths": [],
@@ -52,7 +50,7 @@ if __name__ == "__main__":
             "input_lengths": []
         }
         for current_dataset_index in range(average_dataset_length):
-            original_dataset_index = int(current_dataset_index/len(data_set))
+            original_dataset_index = int(current_dataset_index/len(data_set)) + average_dataset_length*set_number/len(data_set)
             original_dataset_number = current_dataset_index % len(data_set)
 
             if (original_dataset_index >= len(data_set[original_dataset_number]["label_lengths"])):
