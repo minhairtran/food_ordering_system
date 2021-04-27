63c862864871bd0bcfd88fdc5f78e88a63af265b
Trần Minh Hải <minhairtran@gmail.com>
2021-04-27 22:11:21 +0700

    train model confirming

--- train/model.py
+++ train/model.py
@@ -89,7 +89,7 @@ class SpeechRecognitionModel(nn.Module):
         "stride": 2,
         "learning_rate": 5e-4,
         "batch_size": 4,
-        "epochs": 10, 
+        "epochs": 20, 
         "test_size": 0.2
     }
     
