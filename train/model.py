6de59a8a360b7a19f18dcf718ae35441a8e006ca
Trần Minh Hải <minhairtran@gmail.com>
2021-04-29 18:16:57 +0700

    fix bug

--- train/model.py
+++ train/model.py
@@ -165,7 +165,7 @@ class ConfirmingModel(nn.Module):
         self.cnn_layers.add_module("conv_3", CNN(in_channels=64, out_channels=64, kernel=3, stride=1, n_feats=n_feats))
 
         self.classifier = nn.Sequential(
-            nn.Linear(n_feats*64, n_feats)
+            nn.Linear(n_feats*64, n_feats),
             nn.GELU(),
             nn.Dropout(dropout),
             nn.Linear(n_feats, n_class)
