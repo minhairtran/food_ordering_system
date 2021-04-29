95411d5dd509a58419b7642a369c84088f2e0830
Trần Minh Hải <minhairtran@gmail.com>
2021-04-29 14:12:41 +0700

    confirming model one hot decoding

--- data/prepare_data_confirming.py
+++ data/prepare_data_confirming.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 import torch
 import librosa
 import os
-from train.text_transform import ConfirmTextTransform
 import numpy as np
 import json
 
@@ -14,15 +13,12 @@ SAVED_FILE = "confirming_data/data.json"
 
 def preprocess_dataset(dataset_path, saved_file_path, n_mels=20, n_fft=512, hop_length=384):
     saved_data = []
+    dataset_number = 0
 
     for data_set in dataset_path:
-
-        # dictionary where we'll store mapping, labels, MFCCs and filenames
         data = {
-            "label_lengths": [],
             "mel_spectrogram": [],
             "labels": [],
-            "input_lengths": []
         }
 
         # loop through all sub-dirs
@@ -43,19 +39,15 @@ def preprocess_dataset(dataset_path, saved_file_path, n_mels=20, n_fft=512, hop_
                                                 hop_length=hop_length)
 
                 mel_spectrogram = librosa.power_to_db(mel_spectrogram)
-                
-                text_transform = ConfirmTextTransform()
-
-                added_label = text_transform.text_to_int(label)
 
                 # store data for analysed track
                 data["mel_spectrogram"].append(mel_spectrogram.T.tolist())
-                data["labels"].append(added_label)
-                data["input_lengths"].append(mel_spectrogram.T.shape[0]//2)
-                data["label_lengths"].append(len(label))
-                print("{}: {}".format(file_path, i-1))
+                data["labels"].append(dataset_number)
+                print("{}: {}".format(file_path, dataset_number))
 
             saved_data.append(data)
+        
+        dataset_number += 1
 
     with open(saved_file_path, 'w') as f:
         json.dump(saved_data, f, indent=4)
