cfa40527045ced6c07149028c1525edc349dcac3
Trần Minh Hải <minhairtran@gmail.com>
2021-04-29 17:55:51 +0700

    fix bug

--- train/model.py
+++ train/model.py
@@ -163,15 +163,17 @@ class ConfirmingModel(nn.Module):
         self.cnn_layers.add_module("conv_1", CNN(in_channels=1, out_channels=128, kernel=3, stride=1, n_feats=n_feats))
         self.cnn_layers.add_module("conv_2", CNN(in_channels=128, out_channels=64, kernel=3, stride=1, n_feats=n_feats))
         self.cnn_layers.add_module("conv_3", CNN(in_channels=64, out_channels=64, kernel=3, stride=1, n_feats=n_feats))
+
+        self.fully_connected = nn.Linear(n_feats*64, n_feats)
         
         self.classifier = nn.Sequential(
-            nn.Flatten(),  # birnn returns rnn_dim*2
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(n_feats*64, n_class)
+            nn.Linear(n_feats, n_class)
         )
 
     def forward(self, x):
         x = self.cnn_layers(x)
+        x = self.fully_connected(x)
         x = self.classifier(x)
         return x
