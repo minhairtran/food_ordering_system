1ee1aa53610696a417abf9d6ef510a81b820df14
minhairtran <minhairtran@gmail.com>
2021-05-02 16:22:01 +0700

    fix bug

--- train/model.py
+++ train/model.py
@@ -146,6 +146,7 @@ class ConfirmingModel(nn.Module):
 
     def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
         super(ConfirmingModel, self).__init__()
+        n_feats = n_feats//2
         self.cnn = nn.Conv2d(1, 32, 3, stride=stride, padding=3//2)  # cnn for extracting heirachal features
 
         # n residual cnn layers with filter size of 32
