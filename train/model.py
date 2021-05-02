c5e8bf075cd327591b0b35d523e97bc316a1954a
minhairtran <minhairtran@gmail.com>
2021-05-02 17:22:28 +0700

    train model confirming

--- train/model.py
+++ train/model.py
@@ -139,7 +139,7 @@ class ConfirmingModel(nn.Module):
         "stride": 2,
         "learning_rate": 5e-4,
         "batch_size": 4,
-        "epochs": 10, 
+        "epochs": 30, 
         "test_size": 0.2,
         "n_class": 1
     }
