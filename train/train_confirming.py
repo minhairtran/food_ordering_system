f3828a8c595ede9535bab9503a9a68def323e4a2
Trần Minh Hải <minhairtran@gmail.com>
2021-05-04 19:58:52 +0700

    fix bug confirming model

--- train/train_confirming.py
+++ train/train_confirming.py
@@ -180,14 +180,16 @@ if __name__ == "__main__":
 
     load_data_set = torch.load(DATA_PATH)
 
-    for dataset_index in range(len(load_data_set)):
-        # Load all data
-        mel_spectrogram, labels = load_data(load_data_set[dataset_index])
+    
+    for epoch in range(1, ConfirmingModel.hparams["epochs"] + 1):
+        
+        for dataset_index in range(len(load_data_set)):
+            # Load all data
+            mel_spectrogram, labels = load_data(load_data_set[dataset_index])
 
-        # Split into train and test
-        mel_spectrogram_train, mel_spectrogram_test, labels_train, labels_test = train_test_split(mel_spectrogram, labels, test_size=ConfirmingModel.hparams['test_size'], shuffle=False)
+            # Split into train and test
+            mel_spectrogram_train, mel_spectrogram_test, labels_train, labels_test = train_test_split(mel_spectrogram, labels, test_size=ConfirmingModel.hparams['test_size'], shuffle=False)
 
-        for epoch in range(1, ConfirmingModel.hparams["epochs"] + 1):
             # Create train dataset and Dataloader
             train_dataset = Dataset(
                 mel_spectrogram_train, labels_train)
