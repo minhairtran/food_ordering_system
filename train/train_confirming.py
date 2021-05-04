ee2023d96c6601dbb44c9ba5bf2fd2d587f58816
Trần Minh Hải <minhairtran@gmail.com>
2021-05-04 19:22:01 +0700

    fix bug confirming model

--- train/train_confirming.py
+++ train/train_confirming.py
@@ -93,6 +93,8 @@ def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, i
             train_precision = precision_score(np.array(label_pred), np.array(labels.tolist()), average='micro')
             train_precision_average.append(train_precision)
 
+            print(train_precision_average)
+
             optimizer.step()
             scheduler.step()
             iter_meter.step()
