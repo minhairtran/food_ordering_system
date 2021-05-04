dd8d22aefa0812d442b66441cfd371995973ddbb
Trần Minh Hải <minhairtran@gmail.com>
2021-05-04 19:18:25 +0700

    fix bug confirming model

--- train/train_confirming.py
+++ train/train_confirming.py
@@ -97,7 +97,7 @@ def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, i
             scheduler.step()
             iter_meter.step()
             if batch_idx % 100 == 0 or batch_idx == data_len:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tPrecision: {:.42}%'.format(
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tPrecision: {:.2}%'.format(
                     epoch, batch_idx * len(spectrograms), data_len,
                     100. * batch_idx / len(train_loader), loss.item(), 100*np.mean(train_precision_average)))
 
