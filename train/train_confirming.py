7ad5e9eeeed35b654602342063bdf7badb5b7ce6
Trần Minh Hải <minhairtran@gmail.com>
2021-05-05 22:20:14 +0700

    confirming model

--- train/train_confirming.py
+++ train/train_confirming.py
@@ -14,10 +14,12 @@ import torch
 from error_calculating import ErrorCalculating
 from model import ConfirmingModel
 from sklearn.metrics import precision_score
+from text_transform import ConfirmTextTransform
 
 DATA_PATH = "../data/confirming_data/data.pt"
 SAVED_MODEL_PATH = "model_confirming.h5"
 error_calculating = ErrorCalculating()
+text_transform = ConfirmTextTransform()
 
 class GetOutOfLoop( Exception ):
     pass
@@ -25,10 +27,12 @@ class GetOutOfLoop( Exception ):
 class Dataset(torch.utils.data.Dataset):
     'Characterizes a dataset for PyTorch'
 
-    def __init__(self, mel_spectrogram, labels):
+    def __init__(self, mel_spectrogram, labels, input_lengths, label_lengths):
         'Initialization'
         self.mel_spectrogram = mel_spectrogram
         self.labels = labels
+        self.label_lengths = label_lengths
+        self.input_lengths = input_lengths
 
     def __len__(self):
         'Denotes the total number of samples'
@@ -39,8 +43,27 @@ class Dataset(torch.utils.data.Dataset):
         # Select sample
         mel_spectrogram = self.mel_spectrogram[index]
         labels = self.labels[index]
+        label_length = self.label_lengths[index]
+        input_length = self.input_lengths[index]
 
-        return (mel_spectrogram, labels)
+        return (mel_spectrogram, labels, input_length, label_length)
+
+def GreedyDecoder(output, labels, label_lengths, blank_label=0, collapse_repeated=True):
+    arg_maxes = torch.argmax(output, dim=2)
+    decodes = []
+    targets = []
+
+    for i, args in enumerate(arg_maxes):
+        decode = []
+        targets.append(text_transform.int_to_text(
+            labels[i][:label_lengths[i]].tolist()))
+        for j, index in enumerate(args):
+            if index != blank_label:
+                if collapse_repeated and j != 0 and index == args[j - 1]:
+                    continue
+                decode.append(index.item())
+        decodes.append(text_transform.int_to_text(decode))
+    return decodes, targets
 
 class IterMeter(object):
     """keeps track of total iterations"""
@@ -58,86 +81,76 @@ class IterMeter(object):
 def load_data(data):
     mel_spectrogram = data["mel_spectrogram"]
     labels = data["labels"]
+    label_lengths = list(map(int, data["label_lengths"].tolist())) 
+    input_lengths = list(map(int, data["input_lengths"].tolist())) 
 
-    return mel_spectrogram, labels
-
-def decoder(output):
-    arg_maxes = torch.argmax(output, dim=1).tolist()
-    return arg_maxes
+    return mel_spectrogram, labels, input_lengths, label_lengths
 
 def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment):
     model.train()
     data_len = len(train_loader.dataset)
-    train_precision_average = []
     with experiment.train():
         for batch_idx, _data in enumerate(train_loader):
-            spectrograms, labels = _data
+            spectrograms, labels, input_lengths, label_lengths = _data
             spectrograms, labels = spectrograms.to(device), labels.to(
                 device)  # spectro (batch, cnn_feature, n_class, time)
 
             optimizer.zero_grad()
 
             output = model(spectrograms)  # (batch, time, n_class)
-            output = F.log_softmax(output, dim=1)
-            # output = output.transpose(0, 1)  # (time, batch, n_class)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1)  # (time, batch, n_class)
 
-            loss = criterion(output, labels)
+            loss = criterion(output, labels, input_lengths, label_lengths)
             loss.backward()
 
             experiment.log_metric('loss', loss.item(), step=iter_meter.get())
             experiment.log_metric(
                 'learning_rate', scheduler.get_last_lr(), step=iter_meter.get())
-            
-            label_pred = decoder(output)
-
-            train_precision = precision_score(np.array(label_pred), np.array(labels.tolist()), average='micro')
-            train_precision_average.append(train_precision)
 
             optimizer.step()
             scheduler.step()
             iter_meter.step()
-            if (batch_idx % 5 == 0 and batch_idx != 0) or batch_idx == data_len:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tPrecision: {:.2f}%'.format(
+            if batch_idx % 100 == 0 or batch_idx == data_len:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(spectrograms), data_len,
-                    100. * batch_idx / len(train_loader), loss.item(), 100*np.mean(train_precision_average)))
+                    100. * batch_idx / len(train_loader), loss.item()))
 
 
 def test(model, device, test_loader, criterion, iter_meter, experiment, filename):
     print('\nEvaluating ' + str(filename) + "...")
     model.eval()
     test_loss = 0
+    # test_cer, test_wer = [], []
     test_cer = []
-
-    test_precision_average = []
 
     with experiment.test():
         with torch.no_grad():
             for i, _data in enumerate(test_loader):
-                spectrograms, labels= _data
+                spectrograms, labels, input_lengths, label_lengths = _data
                 spectrograms, labels = spectrograms.to(
                     device), labels.to(device)
 
                 output = model(spectrograms)  # (batch, time, n_class)
-                output = F.log_softmax(output, dim=1)
+                output = F.log_softmax(output, dim=2)
+                output = output.transpose(0, 1)  # (time, batch, n_class)
 
-                loss = criterion(output, labels)
+                loss = criterion(output, labels, input_lengths, label_lengths)
                 test_loss += loss.item() / len(test_loader)
 
-                label_pred = decoder(output)
+                decoded_preds, decoded_targets = GreedyDecoder(
+                    output.transpose(0, 1), labels, label_lengths)
+                for j in range(len(decoded_preds)):
+                    test_cer.append(error_calculating.cer(decoded_targets[j], decoded_preds[j]))
 
-                label_pred[0] = round(label_pred[0], 10)
-
-                test_precision = precision_score(np.array(label_pred), np.array(labels.tolist()), average='micro')
-                test_precision_average.append(test_precision)
-
+    avg_cer = sum(test_cer)/len(test_cer)
     experiment.log_metric('test_loss', test_loss, step=iter_meter.get())
-    experiment.log_metric('test_precision', np.mean(test_precision_average), step=iter_meter.get())
+    experiment.log_metric('cer', avg_cer, step=iter_meter.get())
 
-    print('Test set: Average loss: {:.4f}\tTest precision: {:.2f}%\n'.format(
-        test_loss, 100*np.mean(test_precision_average)))
+    print('Test set: Average loss: {:.4f}, Average CER: {:4f}\n'.format(
+        test_loss, avg_cer))
 
-    return np.mean(test_precision_average)
-
+    return avg_cer
 
 if __name__ == "__main__":
 
@@ -158,8 +171,8 @@ if __name__ == "__main__":
     torch.manual_seed(7)
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    model = ConfirmingModel(ConfirmingModel.hparams['n_cnn_layers'], ConfirmingModel.hparams['n_rnn_layers'], ConfirmingModel.hparams['rnn_dim'], ConfirmingModel.hparams['n_class'], ConfirmingModel.hparams['n_feats'], \
-        ConfirmingModel.hparams['stride'], ConfirmingModel.hparams['dropout']).to(device)
+    model = SpeechRecognitionModel(SpeechRecognitionModel.hparams_confirming['n_cnn_layers'], SpeechRecognitionModel.hparams_confirming['n_rnn_layers'], SpeechRecognitionModel.hparams_confirming['rnn_dim'], \
+        9, SpeechRecognitionModel.hparams_confirming['n_feats'], SpeechRecognitionModel.hparams['stride'], SpeechRecognitionModel.hparams_confirming['dropout']).to(device)
 
     try:
         checkpoint = torch.load(SAVED_MODEL_PATH)
@@ -173,7 +186,7 @@ if __name__ == "__main__":
         [param.nelement() for param in model.parameters()]))
 
     optimizer = optim.AdamW(model.parameters(), ConfirmingModel.hparams["learning_rate"])
-    criterion = nn.CrossEntropyLoss().to(device)
+    criterion = nn.CTCLoss(blank=0).to(device)
 
     iter_meter = IterMeter()
 
@@ -182,28 +195,32 @@ if __name__ == "__main__":
     precision = 0
     try:
         for epoch in range(1, ConfirmingModel.hparams["epochs"] + 1):
-            epoch_precisions = []
+            epoch_cer = []
+
             for dataset_index in range(len(load_data_set)):
                 # Load all data
-                mel_spectrogram, labels = load_data(load_data_set[dataset_index])
+                mel_spectrogram, labels, input_lengths, label_lengths = load_data(load_data_set[dataset_index])
 
-                # Split into train and test
-                mel_spectrogram_train, mel_spectrogram_test, labels_train, labels_test = train_test_split(mel_spectrogram, labels, test_size=ConfirmingModel.hparams['test_size'], shuffle=False)
+            # Split into train and test
+                mel_spectrogram_train, mel_spectrogram_test, labels_train, labels_test, input_lengths_train, \
+                    input_lengths_test, label_lengths_train, label_lengths_test = train_test_split(mel_spectrogram, labels,
+                                                                                            input_lengths, label_lengths, test_size=ConfirmingModel.hparams['test_size'], shuffle=False)
 
                 # Create train dataset and Dataloader
-                train_dataset = Dataset(
-                    mel_spectrogram_train, labels_train)
+                train_dataset = Dataset(mel_spectrogram_train, labels_train, input_lengths_train, label_lengths_train)
 
                 train_loader = data.DataLoader(dataset=train_dataset,
                                             batch_size=ConfirmingModel.hparams["batch_size"],
                                             shuffle=True if epoch>10 else False)
 
                 # Create test dataset and Dataloader
-                test_dataset = Dataset(mel_spectrogram_test, labels_test)
+                test_dataset = Dataset(mel_spectrogram_test, labels_test, input_lengths_test, label_lengths_test)
 
                 test_loader = data.DataLoader(dataset=test_dataset,
                                             batch_size=ConfirmingModel.hparams["batch_size"],
                                             shuffle=False)
+
+                print(int(len(train_loader)))
 
                 scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=ConfirmingModel.hparams["learning_rate"],
                                                         steps_per_epoch=int(
@@ -214,11 +231,11 @@ if __name__ == "__main__":
                 train(model, device, train_loader, criterion, optimizer,
                         scheduler, epoch, iter_meter, experiment)
 
-                precision = test(model, device, test_loader, criterion, iter_meter, experiment, dataset_index)
+                cer_test = test(model, device, test_loader, criterion, iter_meter, experiment, dataset_index)
 
-                epoch_precisions.append(precision)
+                epoch_cer.append(cer_test)
 
-            if all(epoch_precision > 0.97 for epoch_precision in epoch_precisions):
+            if all(each_cer < 0.01 for each_cer in epoch_cer):
                 raise GetOutOfLoop
 
     except GetOutOfLoop:
