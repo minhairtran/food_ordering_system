42c3cd00f993aa4dabc86230907b0be51dc33df5
Trần Minh Hải <minhairtran@gmail.com>
2021-05-05 18:50:23 +0700

    fix bug food number model

--- train/model.py
+++ train/model.py
@@ -125,7 +125,7 @@ class FoodNumberModel(nn.Module):
         "dropout": 0.25,
         "stride": 2,
         "learning_rate": 5e-4,
-        "batch_size": 10,
+        "batch_size": 128,
         "epochs": 100, 
         "test_size": 0.1,
         "n_class": 1
