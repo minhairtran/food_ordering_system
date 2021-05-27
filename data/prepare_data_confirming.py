07bd526ee2515150cf06b615d00ae53b1244698d
minhairtran <minhairtran@gmail.com>
2021-05-27 16:09:30 +0700

    confirming model

--- data/prepare_data_confirming.py
+++ data/prepare_data_confirming.py
@@ -60,6 +60,7 @@ def preprocess_dataset(dataset_path, saved_file_path):
                 x = log_mel_spec(a.clone())
 
                 data_temporary["mel_spectrogram"].append(x.T.tolist())
+                data_temporary["labels"].append(dataset_number)
 
                 for i in range(24):
                     mel_spectrogram = np.array(spec_augment(a.clone().unsqueeze(0)).squeeze(0)).T.tolist()
