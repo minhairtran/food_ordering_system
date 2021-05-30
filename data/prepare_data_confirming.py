7db0542c433447b453545fef088d553a32b31de1
minhairtran <minhairtran@gmail.com>
2021-05-30 19:13:50 +0700

    train confirming

--- data/prepare_data_confirming.py
+++ data/prepare_data_confirming.py
@@ -9,7 +9,7 @@ import torchaudio
 import augment
 from scipy.io import wavfile
 
-DATASET_PATH = ["../../confirming_dataset/co", "../../confirming_dataset/khong", "../../confirming_dataset/khong_biet"]
+DATASET_PATH = ["../../confirming_dataset/co", "../../confirming_dataset/khong"]
 # DATASET_PATH = ["../../confirming_with_noise_dataset/co", "../../confirming_with_noise_dataset/khong"]
 SAVED_FILE = "confirming_data/data.json"
 
